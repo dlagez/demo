87d71c400312398327834f038776e3c7f54fe476
UF101233 <Peng.Zhang.cc@partner.bmw-brilliance.cn>
2023-03-22 20:36:44 +0800

    add springboot security module

--- src/main/java/com/example/demo/controller/demo.java
+++ src/main/java/com/example/demo/controller/demo.java
@@ -3,7 +3,7 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/demo")
 public class demo {
 
     @GetMapping("/hello")
